5524247a951fab877ae5d106a95c520341ebf5a5
Carolina Menezes <carolina.menezes.dev@gmail.com>
2025-11-20 19:29:50 -0300

    ateração opções de modelo LLM

--- baixar_modelos.py
+++ baixar_modelos.py
@@ -7,14 +7,14 @@ os.makedirs(DESTINO, exist_ok=True)
 
 # Lista de modelos GGUF que queremos baixar
 modelos = [
-    {
-        "repo": "TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF",
-        "arquivo": "tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf"
-    },
-    {
-        "repo": "TheBloke/phi-2-GGUF",
-        "arquivo": "phi-2.Q4_K_M.gguf"
-    }, 
+    #{
+    #    "repo": "TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF",
+    #    "arquivo": "tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf"
+    #},
+   # {
+   #     "repo": "TheBloke/phi-2-GGUF",
+    #    "arquivo": "phi-2.Q4_K_M.gguf"
+   # }, 
     {
        "repo": "cnmoro/Gemma-3-Gaia-PT-BR-4b-it-Q4_K_M-GGUF",
         "arquivo": "gemma-3-gaia-pt-br-4b-it-q4_k_m.gguf"
